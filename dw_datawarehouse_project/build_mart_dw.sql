cd4f75e92108772625aa42b2424d9142b99472ed
Syed Abrash Shah <abrashshah212@gmail.com>
2026-07-16 15:33:56 +0500

    update inserted data

--- dw_datawarehouse_project/build_mart_dw.sql
+++ dw_datawarehouse_project/build_mart_dw.sql
@@ -1,5 +1,6 @@
+-- duckdb dw_marts.duckdb -c ".read build_mart_dw.sql"
 --- create schema tables
 .read create_tables_dw.sql
 
 --- load data from csv files into tables
-.read create_tables_dw.sql
+.read load_schema_dw.sql
